b5c462c861b44cf54d4732c691d9f2503f3e31c3
TAnga-AE <emmanueltangaassah@gmail.com>
2024-10-15 15:00:25 +0200

    Update fractales.cpp

--- fractales.cpp
+++ fractales.cpp
@@ -1,63 +1,39 @@
 #include <iostream>
-#include <string>
+#include <complex>
 
+void computeMandelbrot(double cx, double cy, int iterations) {
+    std::complex<double> z(0, 0); // Initialisation de z avec z0 = 0
+    std::complex<double> c(cx, cy); // Initialisation de c avec les coordonnées
 
-
-float add(int n, float zn, float z_r, float iz_i, float c_r) {
-
-        zn = z_r*z_r - iz_i*iz_i + c_r;
-
-        return zn;
-    }
-
-float mult(int n, float zni, float z_r, float iz_i, float ic_i) {
-
-        zni =  2*iz_i*z_r + ic_i;
-
-        return zni;
-    }
-
-
-float module(float mod, float z_r, float iz_i) {
-
-        mod = z_r*z_r + iz_i*iz_i;
-         return mod;
-
-    }
+    std::cout << "Suite de Mandelbrot pour C(" << cx << ", " << cy << "):\n";
     
-int main(int argc, char**argv)
- {
-    float mod, c_r, ic_i, z_r,  iz_i, z0, zn, zni, z;
-    unsigned int n = 0;
-
-
-std::cout <<"Entrer n :";
-std::cin >> n;
-/* std::cout<<"Entrer z_r :";
-std::cin >> z_r;
-std::cout<<"Entrer z_i :";
-std::cin >> z_i; */
-std::cout<<"Entrer  c_r :";
-std::cin >> c_r;
-std::cout<<"Entrer  ic_i :";
-std::cin >>ic_i;
-
-
-for(int i = 0; i < n; i++){
-
-    while (module(mod, z_r = 0, iz_i = 0) < 4){
-
-add(n, zni, z_r = 0, iz_i = 0, c_r);
-
-( n, zni, z_r = 0, iz_i = 0,  ic_i);
-
-    z_r = c_r;
-
-    iz_i = ic_i;
-    
+    for (int n = 0; n < iterations; ++n) {
+        // Calculer le module de z
+        double modulus = std::norm(z); // |z|^2 = z_r^2 + z_i^2
+        if (modulus >= 4) {
+            break; // Si le module est supérieur ou égal à 4, on sort de la boucle
+        }
+        
+        std::cout << "z(" << n << ") = (" << z.real() << ", " << z.imag() << ")\n";
+        
+        // Calculer zn+1
+        std::complex<double> zn_next = z * z + c; // zn+1 = zn^2 + c
+        z = zn_next; // Mettre à jour z
     }
-      } 
+}
 
+int main() {
+    double cx, cy;
+    int n;
+
+    std::cout << "Entrez les coordonnées de C (cx, cy): ";
+    std::cin >> cx >> cy;
+    std::cout << "Entrez le nombre d'itérations (n): ";
+    std::cin >> n;
+
+    computeMandelbrot(cx, cy, n);
+    
+    return 0;
 }
 
     
